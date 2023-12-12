cb9e4a2fd9174c16b2d533a5abe942484c06c4c4
RaunoPaluvee <rauno.paluvee@guardtime.com>
2023-12-12 15:58:05 +0200

    day8 and day9

--- 2023/src/Day8.js
+++ 2023/src/Day8.js
@@ -1,13 +1,73 @@
 const { readFileToList } = require('./FileReader.js');
 
-const INPUT_LIST = readFileToList("../input/day6.txt");
+const INPUT_LIST = readFileToList("../input/day8.txt");
+const REGEX = /([\w\d]{3}) = \(([\w\d]{3}), ([\w\d]{3})\)/;
 
 const resolvePartOne = () => {
-
+  const instructions = INPUT_LIST[0].split("");
+  let map = {};
+  for (let line of INPUT_LIST.slice(2)) {
+    let node = line.match(REGEX)[1];
+    let left = line.match(REGEX)[2];
+    let right = line.match(REGEX)[3];
+    map[node] = [left, right];
+  }
+  let curr = "AAA";
+  let count = 0;
+  while (curr !== "ZZZ") {
+    for (let instruction of instructions) {
+      curr = map[curr][instruction === "L" ? 0 : 1];
+      count++;
+      if (curr === "ZZZ") break;
+     }
+  }
+  return count;
 }
 
 const resolvePartTwo = () => {
-  
+  const instructions = INPUT_LIST[0].split("");
+  let map = {};
+  for (let line of INPUT_LIST.slice(2)) {
+    let node = line.match(REGEX)[1];
+    let left = line.match(REGEX)[2];
+    let right = line.match(REGEX)[3];
+    map[node] = [left, right];
+  }
+  const startingNodes = Object.entries(map)
+      .filter(([key, value]) => key.endsWith("A"))
+      .map(([key, value]) => key);
+
+  let counts = [];
+  for (let node of startingNodes) {
+    let count = 0;
+    let resultNode = node;
+    while (!resultNode.endsWith("Z")) {
+      for (let instruction of instructions) {
+        resultNode = map[resultNode][instruction === "L" ? 0 : 1];
+        count++;
+        if (resultNode.endsWith("Z")) {
+          counts.push(count);
+          break;
+        }
+      }
+    }
+  }
+  return lcm(...counts);
+}
+
+const lcm = (...numbers) => {
+  return numbers.reduce((a, b) => a * b / gcd(a, b));
+}
+
+const gcd = (...numbers) => {
+  return numbers.reduce((a, b) => {
+      while (b) {
+          let t = b;
+          b = a % b;
+          a = t;
+      }
+      return a;
+  });
 }
 
 console.log(resolvePartOne());
--- 2023/src/Day9.js
+++ 2023/src/Day9.js
@@ -1,13 +1,70 @@
 const { readFileToList } = require('./FileReader.js');
 
-const INPUT_LIST = readFileToList("../input/day6.txt");
+const INPUT_LIST = readFileToList("../input/day9.txt");
+
+class Node {
+  constructor(left, right, parent) {
+    this.left = left;
+    this.right = right;
+    this.parent = parent;
+  }
+}
+
+const buildNodes = (line) => {
+  const values = line.split(" ").map(val => Number(val));
+  let nodes = []; // leaf nodes
+  for (let i = 0; i < values.length - 1; i++) {
+    const val = values[i];
+    const nextVal = values[i + 1];
+    nodes.push(new Node(val, nextVal, nextVal - val))
+  }
+
+  let allNodes = []; // all levels in tree
+  while (nodes.some(node => node.parent !== 0)) {
+    let nextNodes = [];
+    for (let i = 0; i < nodes.length - 1; i++) {
+      const val = nodes[i].parent;
+      const nextVal = nodes[i + 1].parent;
+      nextNodes.push(new Node(val, nextVal, nextVal - val));
+    }
+    allNodes.push(nodes);
+    nodes = nextNodes;
+  }
+  return allNodes;
+}
 
 const resolvePartOne = () => {
-
+  let result = [];
+  for (let line of INPUT_LIST) {
+    const allNodes = buildNodes(line);
+    let sum = allNodes.reduce((acc, nodeList) => acc + nodeList[nodeList.length - 1].parent, 0)
+    sum += allNodes[0][allNodes[0].length - 1].right;
+    result.push(sum);
+  }
+  return result.reduce((acc, val) => acc + val, 0);
 }
 
 const resolvePartTwo = () => {
-  
+  let result = [];
+  for (let line of INPUT_LIST) {
+    const allNodes = buildNodes(line).reverse();
+    let sum = 0;
+    if (allNodes.length === 1) {
+      sum = allNodes[0][0].parent;
+    }
+    for (let i = 0; i < allNodes.length - 1; i++) {
+      currLevel = allNodes[i];
+      nextLevel = allNodes[i + 1];
+      if (i === 0) {
+        sum = nextLevel[0].parent - currLevel[0].parent;
+      } else {
+        sum = nextLevel[0].parent - sum;
+      }
+    }
+    sum = allNodes[allNodes.length - 1][0].left - sum;
+    result.push(sum);
+  }
+  return result.reduce((acc, val) => acc + val, 0);
 }
 
 console.log(resolvePartOne());
